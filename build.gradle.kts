31d68ec84bc0e90011085f7543713b6251831688
Dustin <dburckh@gmail.com>
2024-12-15 09:02:50 -0700

    Changes for building prefab

---
+++ build.gradle.kts
@@ -0,0 +1,47 @@
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    alias(libs.plugins.android.library)
+    alias(libs.plugins.maven.publish)
+}
+
+android {
+    namespace = "com.homesoft.h264bitstream"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+
+    }
+    buildFeatures {
+        prefab = true
+        prefabPublishing = true
+    }
+
+    prefab {
+        create("h264bitstream") {
+            headers = "include"
+        }
+    }
+    externalNativeBuild {
+        cmake {
+            path("CMakeLists.txt")
+            version = "3.31.1"
+        }
+    }
+    publishing {
+        singleVariant("release")
+    }
+}
+//gradlew h264bitstream:publish
+afterEvaluate {
+    publishing {
+        publications {
+            register("h264bitstream", MavenPublication::class) {
+                from(components["release"])
+                groupId = "com.homesoft.android"
+                artifactId = "h264bitstream"
+                version = "0.2.0"
+            }
+        }
+    }
+}
